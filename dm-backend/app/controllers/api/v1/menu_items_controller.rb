f8328e277fc53d5f74c36beffb5a3c4ae72006ed
kchohee <56492609+kchohee@users.noreply.github.com>
2020-10-14 20:39:37 -0500

    created  menu items

---
+++ dm-backend/app/controllers/api/v1/menu_items_controller.rb
@@ -0,0 +1,3 @@
+class Api::V1::MenuItemsController < ApplicationController
+    
+end
---
+++ dm-backend/db/migrate/20201014222713_create_menu_items.rb
@@ -0,0 +1,12 @@
+class CreateMenuItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :menu_items do |t|
+      t.string :title
+      t.string :category
+      t.string :desctiption
+      t.belongs_to :user, foreign_key: {on_delete: :cascade}
+
+      t.timestamps
+    end
+  end
+end
