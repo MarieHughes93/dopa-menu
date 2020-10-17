4326fa5fc7e4073a1decaa3b16c4fe25af64a840
kchohee <56492609+kchohee@users.noreply.github.com>
2020-10-17 16:52:17 -0500

    seed data and schemed for menu item & user

--- dm-backend/db/seeds.rb
+++ dm-backend/db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+MenuItem.destroy_all
+
+users = []
+menu_items = []
+item_types = ['Appetizer','Entree','Side','Dessert','Special']
+
+10.times do
+    User.create do |u|
+        u.name = Faker::GreekPhilosophers.name
+        u.email = Faker::Internet.unique.email
+        u.password_digest = Faker::Internet.password
+        users << u
+    end
+end
+30.times do
+    MenuItem.create do |m|
+        m.title = Faker::Lorem.sentence
+        m.category = item_types.sample
+        m.description = Faker::Lorem.paragraph
+        m.user = users.sample
+    end
+end
+
+tester = User.create(name: "Test", email: "test@test.com",  password_digest: "test")
+test_item1 = MenuItem.create(title: "Test Menu", category: item_types.sample,description: " test test test test test test test", user: tester)
