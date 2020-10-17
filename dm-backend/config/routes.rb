fdb3656a759dd129d37b0e0d238a50faec8f6a22
kchohee <56492609+kchohee@users.noreply.github.com>
2020-10-17 16:51:52 -0500

    user menu and session routes

--- dm-backend/config/routes.rb
+++ dm-backend/config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope module: 'api' do
+    scope module: 'v1' do
+      post "/login", to: "session#login"
+      resources :users, path:'dopa' do
+         resources :menu_items,path:'menu'
+      end
+    end
+  end
+
 end
