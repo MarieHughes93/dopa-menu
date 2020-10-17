5c239dacfc3dd6699837fdfa1428d80c545fd6ed
kchohee <56492609+kchohee@users.noreply.github.com>
2020-10-17 16:51:14 -0500

    menu item controller

--- dm-backend/app/controllers/api/v1/menu_items_controller.rb
+++ dm-backend/app/controllers/api/v1/menu_items_controller.rb
@@ -1,3 +1,40 @@
 class Api::V1::MenuItemsController < ApplicationController
-    
+    before_action :find_user
+    def index
+        @items = @user.menu_items
+        render json: @items
+    end
+    def show
+        @item = MenuItem.find(params[:id])
+        render json: @item
+    end
+    def create
+        @item = MenuItem.new(menu_params)
+        if @item.save
+            render json @item
+        else
+            render json: {error: 'There was an issue creating your menu item. Please try again.'}
+        end
+    end
+    def update
+        @item = MenuItem.find(params[:id])
+        if @item.update(menu_params)
+            render json: @item
+        else
+            render json: {error: 'There was an issue updating your menu item. Please try again.'}
+        end
+    end
+    def destroy
+        @item = MenuItem.find(params[:id])
+        @item.destroy
+        render json: {menu_itemId: @item.id}
+      end
+
+    private
+    def find_user
+        @user = User.find(params[:user_id])
+    end
+    def menu_params
+        params.require(:menu_item).permit( :title, :category, :descripton, :user_id)
+    end
 end
