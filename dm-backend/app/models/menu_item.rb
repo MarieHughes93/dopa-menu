5ed056888a9bae685cb2a605300c8147e3b69a49
kchohee <56492609+kchohee@users.noreply.github.com>
2020-10-17 16:51:36 -0500

    user and menu item model

--- dm-backend/app/models/menu_item.rb
+++ dm-backend/app/models/menu_item.rb
@@ -1,2 +1,4 @@
 class MenuItem < ApplicationRecord
+    belongs_to :user
+    validates_inclusion_of :category, :in => ['Appetizer','Entree','Side','Dessert','Special']
 end
--- dm-backend/app/models/user.rb
+++ dm-backend/app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
-    has_secure_password
+    has_secure_password 
+    has_many :menu_items
+    validates :name, :email, presence: true
 end
