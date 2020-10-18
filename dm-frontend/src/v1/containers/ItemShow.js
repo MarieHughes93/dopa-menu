bd740f3d4fa52a69d5fed58491e2d4c82ae7441c
kchohee <56492609+kchohee@users.noreply.github.com>
2020-10-17 20:45:49 -0500

    made empty files for needed containers

---
+++ dm-frontend/src/v1/containers/ItemShow.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class ItemShow extends Component{
+  render(){
+  return (
+    <div >
+        ItemShow
+    </div>
+  );
+}
+}
+
+export default ItemShow;
