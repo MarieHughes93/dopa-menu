9713426868f381be25587fb547660cb2b2776c87
kchohee <56492609+kchohee@users.noreply.github.com>
2020-10-20 13:51:43 -0500

    created blank form

---
+++ dm-frontend/src/v1/components/signUpForm.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+
+class SignUpForm extends Component {
+    
+    render(){
+        return(
+            <div>
+               <form>
+                   <input
+                    type=""
+                    name=""
+                    placeholder=""
+                    value=""
+                    onChange=""
+                   />
+                   <br/>
+                   <input
+                    type="submit"
+                    value="" 
+                   />
+               </form>
+            </div>
+        )
+    }
+}
+export default SignUpForm
--- dm-frontend/src/v1/containers/SignUp.js
+++ dm-frontend/src/v1/containers/SignUp.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import SignUpForm from '../components/signUpForm'
 
 class SignUp extends Component{
   render(){
   return (
     <div >
-        SignUp
+        <SignUpForm/>
     </div>
   );
 }
