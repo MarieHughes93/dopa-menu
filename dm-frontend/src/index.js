2a33dc7a26e1f577b9eb808250d6de20a6311f9f
kchohee <56492609+kchohee@users.noreply.github.com>
2020-10-20 21:52:11 -0500

    moved store to helpers

--- dm-frontend/src/index.js
+++ dm-frontend/src/index.js
@@ -4,21 +4,14 @@ import './App.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux'
-import {createStore, applyMiddleware, compose} from 'redux'
-import thunk from 'redux-thunk'
 
 import App from './App';
-import reducers from './v1/reducers/_index'
 import { BrowserRouter } from 'react-router-dom';
-
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducers,composeEnhancers((applyMiddleware(thunk))))
+import store from './v1/helpers/store'
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-
     <App />
     </BrowserRouter>
 
---
+++ dm-frontend/src/v1/helpers/store.js
@@ -0,0 +1,13 @@
+
+import {createStore, applyMiddleware, compose} from 'redux'
+import { createLogger } from 'redux-logger'
+import reducers from '../reducers/_index'
+import thunk from 'redux-thunk'
+
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const logger = createLogger()
+
+const store = createStore(reducers,composeEnhancers((applyMiddleware(thunk,logger))))
+ 
+export default store
