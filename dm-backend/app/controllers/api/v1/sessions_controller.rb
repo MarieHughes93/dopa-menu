5f6fd46743f7985d7f43a22315ea9e95fc19fdfa
kchohee <56492609+kchohee@users.noreply.github.com>
2020-10-24 01:00:30 -0500

    session login

--- dm-backend/app/controllers/api/v1/sessions_controller.rb
+++ dm-backend/app/controllers/api/v1/sessions_controller.rb
@@ -1,18 +1,20 @@
 class Api::V1::SessionsController < ApplicationController
-    before_action :find_user
 
     def login
-        # @user = User.find_by(email: params[:user][:email])
-        if @user && @user.authenticate(params[:user][:password])
-            token = encode_token({user_id: @user.id})
-            render json: {user: @user, token: token}
-        else
-            render json: {error: "Invalid email or password"}
-        end
+    @user = User.find_by(email: params[:email])
+    if @user && @user.authenticate(params[:password])
+      token = scramble_tok({user_id: @user.id})
+      render json: {user: @user.id.to_json, token: token}
+    else
+      render json: {error: "Invalid email or password"}
     end
+  end
+
+
     private
-    def find_user
-        @user = User.find(params[:user_id])
-    end
+
+    def user_params
+        params.require(:user).permit(:email, :password)
+      end
 
 end
