773247400254d9d51563686e27bdc6308cf9218e
kchohee <56492609+kchohee@users.noreply.github.com>
2020-10-23 21:55:21 -0500

    correct typo

--- dm-backend/db/migrate/20201014222713_create_menu_items.rb
+++ dm-backend/db/migrate/20201014222713_create_menu_items.rb
@@ -3,7 +3,7 @@ class CreateMenuItems < ActiveRecord::Migration[6.0]
     create_table :menu_items do |t|
       t.string :title
       t.string :category
-      t.string :desctiption
+      t.string :description
       t.belongs_to :user, foreign_key: {on_delete: :cascade}
 
       t.timestamps
--- dm-backend/db/schema.rb
+++ dm-backend/db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2020_10_14_222713) do
   create_table "menu_items", force: :cascade do |t|
     t.string "title"
     t.string "category"
-    t.string "desctiption"
+    t.string "description"
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
