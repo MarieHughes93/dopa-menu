4f02d40b29cb633be35d5a37c2eb21777fda6c26
kchohee <56492609+kchohee@users.noreply.github.com>
2020-10-28 15:54:50 -0500

    add alert to app actions

--- dm-frontend/src/v1/actions/appAction.js
+++ dm-frontend/src/v1/actions/appAction.js
@@ -1,13 +1,13 @@
 import {helpers} from '../helpers/_index'
 import history from '../helpers/history'
 import {actionCreator} from './actionCreators'
+import {alertAction} from './alertAction'
 
 
 export const sessionCheck= () => {
     if (localStorage.getItem("sessionID")) {
         return true
     }
-
     return false
 }
 
@@ -15,21 +15,24 @@ const logInRequest = (user) =>
 ({type: actionCreator.app.LOGIN_REQUEST, user})
 const logInFailure = () => 
 ({type: actionCreator.app.LOGIN_FAILED})
-const logInSuccess = (user) => 
-({type: actionCreator.app.LOGIN_SUCCESS, user})
+const logInSuccess = (token) => 
+({type: actionCreator.app.LOGIN_SUCCESS, token})
 
 export const logIn = (user) => dispatch => {
     dispatch(logInRequest(user))
     helpers.fetch.apiLogin(user)
     .then(
-        user => { 
-            localStorage.setItem("sessionID", user.token)
-            dispatch(logInSuccess(user))
+        data => { 
+            console.log(data)
+            localStorage.setItem("sessionID", data.token)
+            dispatch(logInSuccess(data))
             history.push('/dashboard')
+            dispatch(alertAction.notification(`Welcome`))
         },
-        
         error => {
-            dispatch(logInFailure())
+            console.log(error)
+            dispatch(logInFailure(error))
+            dispatch(alertAction.error(error))
         }
     )
 }
@@ -39,8 +42,8 @@ const logOutRequest = () =>
 ({type: actionCreator.app.LOGOUT_REQUEST})
 const logOutFailure = () => 
 ({type: actionCreator.app.LOGOUT_FAILED})
-const logOutSuccess = () => 
-({type: actionCreator.app.LOGOUT_SUCCESS})
+const logOutSuccess = (error) => 
+({type: actionCreator.app.LOGOUT_SUCCESS,error})
 
 export const logOut = () => dispatch => {
     dispatch(logOutRequest())
@@ -51,7 +54,8 @@ export const logOut = () => dispatch => {
             history.push('/')
         },
         error =>{
-            dispatch(logOutFailure())
+            dispatch(logOutFailure(error))
+            dispatch(alertAction.alertError(error.toString()))
         }
     )
 }
