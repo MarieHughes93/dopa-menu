fb9fc2e9bc7a187cc199a77f57ea97f4d4cb2d51
kchohee <56492609+kchohee@users.noreply.github.com>
2020-10-27 20:00:38 -0500

    updated state to always state a logged status

--- dm-frontend/src/v1/reducers/appReducer.js
+++ dm-frontend/src/v1/reducers/appReducer.js
@@ -4,37 +4,51 @@ let user = localStorage.getItem("sessionID")
 const defaultState = user ? {
     loggedIn: true, 
     user }
- : {}
+ : {loggedIn: false,
+user: null}
 
 
 export const appReducer = (state = defaultState, action) => {
     switch(action.type){
         case 'REQUESTING_LOGIN':
             return {
-                loggingIn: true,
-                loadingUser: action.user
+                ...state,
+                loading: true,
+                loggedIn: false,
+                user: null
             }
         case 'LOGIN_FAILURE':
             return {
-                ...state
+                ...state,
+                loading: false,
+                loggedIn: false,
+                user: null
             }
         case 'LOGIN_SUCCESSFUL':
             return {
+                ...state,
+                loading: false,
                 loggedIn: true,
                 user: action.user
             }
         case 'REQUESTING_LOGOUT':
             return{
-                loggingOut: true,
-                user:{}
+                ...state,
+                loading: true,
+                loggedIn: true
             }
         case 'LOGOUT_FAILURE':
             return{
-                ...state
+                ...state,
+                loading: false,
+                loggedIn: true
             }
         case 'LOGOUT_SUCCESSFULL':
             return {
-                loggedIn: false
+                ...state,
+                loading: false,
+                loggedIn: false,
+                user: null
             }
         default: 
             return state
