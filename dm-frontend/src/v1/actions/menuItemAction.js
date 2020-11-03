a874324b849d942c9228682925cd05e5d31cf9f4
kchohee <56492609+kchohee@users.noreply.github.com>
2020-11-03 16:07:00 -0600

    shell for reducer and action to build on

--- dm-frontend/src/v1/actions/menuItemAction.js
+++ dm-frontend/src/v1/actions/menuItemAction.js
@@ -1,2 +1,105 @@
+// import {actionCreator} from '../actions/actionCreators'
+
+// export const indexRequest = () => ({type: actionCreator.menuItem.INDEX_REQUEST})
+// export const indexSuccess = () => ({type: actionCreator.menuItem.INDEX_SUCCESS})
+// export const indexFailure = () => ({type: actionCreator.menuItem.INDEX_FAILED})
+// export const allItemsFetch = (user) => dispatch => {
+//     dispatch(indexRequest())
+//     helpers.fetch.apiMenuIndex(user)
+//     .then(
+//         data => {
+//             dispatch(indexSuccess())},
+//         error => {
+//             dispatch(indexFailure())
+//     })
+// }
+
+// 
+
+// export const createRequest = () => ({type: actionCreator.menuItem.CREATE_REQUEST})
+// export const createSuccess = () => ({type: actionCreator.menuItem.CREATE_SUCCESS})
+// export const createFailure = () => ({type: actionCreator.menuItem.CREATE_FAILED})
+// export const createItem = (user,menuItem) => dispatch => {
+//     dispatch(createRequest())
+//     helpers.fetch.apiMenuItemCreate(user,menuItem)
+//     .then(
+//         data => {
+//             dispatch(createSuccess())},
+//         error => {
+//             dispatch(createFailure())
+//     })
+// }
+
+// export const fetchRequest = () => ({type: actionCreator.menuItem.FETCH_REQUEST})
+// export const fetchSuccess = () => ({type: actionCreator.menuItem.FETCH_SUCCESS})
+// export const fetchFailure = () => ({type: actionCreator.menuItem.FETCH_FAILED})
+// export const itemFetch = (user, menuItem) => dispatch => {
+//     dispatch(fetchRequest())
+//     helpers.fetch.apiMenuItemShow(user, menuItem)
+//     .then(
+//         data => {
+//             dispatch(fetchSuccess())},
+//         error => {
+//             dispatch(fetchFailure())
+//     })
+// }
+
+// export const updateRequest = () => ({type: actionCreator.menuItem.UPDATE_REQUEST})
+// export const updateSuccess = () => ({type: actionCreator.menuItem.UPDATE_SUCCESS})
+// export const updateFailure = () => ({type: actionCreator.menuItem.UPDATE_FAILED})
+// export const itemUpdate = (user, menuItem) => dispatch => {
+//     dispatch(updateRequest())
+//     helpers.fetch.apiMenuItemUpdate(user, menuItem)
+//     .then(
+//         data => {
+//             dispatch(updateSuccess())},
+//         error => {
+//             dispatch(updateFailure())
+//     })
+// }
+
+// export const deleteRequest = () => ({type: actionCreator.menuItem.DELETE_REQUEST})
+// export const deleteSuccess = () => ({type: actionCreator.menuItem.DELETE_SUCCESS})
+// export const deleteFailure = () => ({type: actionCreator.menuItem.DELETE_FAILED})
+// export const itemDelete = (user, menuItem) => dispatch => {
+//     dispatch(deleteRequest())
+//     helpers.fetch.apiMenuItemDelete(user, menuItem)
+//     .then(
+//         data => {
+//             dispatch(deleteSuccess())},
+//         error => {
+//             dispatch(deleteFailure())
+//     })
+// }
+
+// export const itemSessionEnd=()=>({type: actionCreator.menuItem.SESSION_END})
+
+
 export const  menuItemAction = {
+    // indexRequest,
+    // indexSuccess,
+    // indexFailure,
+    // allItemsFetch,
+
+    // createRequest,
+    // createSuccess,
+    // createFailure,
+    // createItem,
+
+    // fetchRequest,
+    // fetchSuccess,
+    // fetchFailure,
+    // itemFetch,
+
+    // updateRequest,
+    // updateSuccess,
+    // updateFailure,
+    // itemUpdate,
+
+    // deleteRequest,
+    // deleteSuccess,
+    // deleteFailure,
+    // itemDelete,
+
+    // itemSessionEnd
 }
--- dm-frontend/src/v1/reducers/menuItemReducer.js
+++ dm-frontend/src/v1/reducers/menuItemReducer.js
@@ -3,30 +3,72 @@ import {actions} from '../actions/_index'
 
 export const menuItemReducer = (state = {}, action) => {
     switch(action.type){
+
+        // index
         case actions.creator.menuItem.INDEX_REQUEST:
-            return {}
+            return {
+                ...state,
+                loading: true}
         case actions.creator.menuItem.INDEX_SUCCESS:
-            return {}
+            return {
+            }
         case actions.creator.menuItem.INDEX_FAILED:
-            return {}
+            return {
+                ...state,
+                loading: false}
+        
+        // create
         case actions.creator.menuItem.CREATE_REQUEST:
-            return {}
+            return {
+                ...state,
+                loading: true}
         case actions.creator.menuItem.CREATE_SUCCESS:
             return {}
         case actions.creator.menuItem.CREATE_FAILED:
-            return {}                    
-        case actions.creator.menuItem.UPDATE_REQUEST:
+            return {
+                ...state,
+                loading: false}
+
+        // fetch/show
+        case actions.creator.menuItem.FETCH_REQUEST:
+            return {
+                ...state,
+                loading: true}
+        case actions.creator.menuItem.FETCH_SUCCESS:
             return {}
+        case actions.creator.menuItem.FETCH_FAILED:
+            return {
+                ...state,
+                loading: false}
+
+        // update
+        case actions.creator.menuItem.UPDATE_REQUEST:
+            return {
+                ...state,
+                loading: true}
         case actions.creator.menuItem.UPDATE_SUCCESS:
             return {}
         case actions.creator.menuItem.UPDATE_FAILED:
-            return {}
+            return {
+                ...state,
+                loading: false}
+
+        // delete
         case actions.creator.menuItem.DELETE_REQUEST:
-            return {}
-        case actions.creator.menuItem.DELTE_SUCCESS:
+            return {
+                ...state,
+                loading: true}
+        case actions.creator.menuItem.DELETE_SUCCESS:
             return {}
         case actions.creator.menuItem.DELETE_FAILED:
+            return {
+                ...state,
+                loading: false}
+
+        // end session
+        case actions.creator.menuItem.SESSION_END:
             return {}
+
     default: 
             return state
     }
