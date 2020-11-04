23c76b76ea83c0be165a3010ff14d73a153c5ab1
kchohee <56492609+kchohee@users.noreply.github.com>
2020-11-03 19:56:38 -0600

    user profile working

--- dm-frontend/src/v1/components/user.js
+++ dm-frontend/src/v1/components/user.js
@@ -1,11 +1,11 @@
 import React  from 'react'
 
-export const User =(props) =>(
-    <div></div>
-/* <div className="userContainer" key={props.user.id}>
-        <h1>Hello, {props.user.name}</h1>
-        <p><strong>Email:</strong>{props.user.email}</p>
-        <button onClick={props.toggleEdit} className="btn btn-default  ">edit</button>
-        <button onClick={props.deleteuser} className="btn btn-default  ">delete</button>
-      </div> */
+export const User =({user, toggleEdit, deleteUser }) =>(
+    
+    <div className="userContainer" key={user.id}>
+        <h1>Hello, {user.name}</h1>
+        <p><strong>Email:</strong>{user.email}</p>
+        <button onClick={toggleEdit} className="btn btn-default  ">edit</button>
+        <button onClick={()=>deleteUser(user)} className="btn btn-default  ">delete</button>
+    </div>
 )
