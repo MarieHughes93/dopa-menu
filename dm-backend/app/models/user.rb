dd0c61459f1651ac7ca256fe79f10efcfc78db48
kchohee <56492609+kchohee@users.noreply.github.com>
2020-11-06 11:10:52 -0600

    update functional.

--- dm-backend/app/models/user.rb
+++ dm-backend/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
     validates_presence_of :name, :email
-    validates_uniqueness_of :email
+    validates_uniqueness_of :email, on: create
     validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
 end
