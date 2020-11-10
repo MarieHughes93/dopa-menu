9422804c9359fdc3a3a633b5ff8a3f3a46738dd6
kchohee <56492609+kchohee@users.noreply.github.com>
2020-11-10 13:55:13 -0600

    changed to menuItem

--- dm-frontend/src/v1/actions/_index.js
+++ dm-frontend/src/v1/actions/_index.js
@@ -2,13 +2,13 @@
 import {actionCreator as creator} from './actionCreators'
 import {appAction as app } from './appAction'
 import {alertAction as alert} from './alertAction'
-import {menuItemAction as menu} from './menuItemAction'
+import {menuItemAction as menuItem} from './menuItemAction'
 import {userAction as user} from './userAction'
 
 export const actions ={
     creator,
     app,
     alert,
-    menu,
+    menuItem,
     user
 }
