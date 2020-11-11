c21bedbb364f0dc9f1eb7c5b41624cc646265a63
kchohee <56492609+kchohee@users.noreply.github.com>
2020-11-10 20:54:33 -0600

    changed menu items to item on singular and nil

--- dm-backend/app/controllers/api/v1/menu_items_controller.rb
+++ dm-backend/app/controllers/api/v1/menu_items_controller.rb
@@ -8,42 +8,42 @@ class Api::V1::MenuItemsController < ApplicationController
 
     def show
         if @item= @user.menu_items.find(params[:id])
-            render json: {menuItems: @item, user: @user, auth: true, error: false, heading: "Fetch Complete", message: "Item's info retrieved"}
+            render json: {menuItem: @item, user: @user, auth: true, error: false, heading: "Fetch Complete", message: "Item's info retrieved"}
         else
-            render json: {menuItems: null, user: @user, auth: true , error: true , heading: "Opps...", message: "We ran into an error loading your selection. Please try again."}
+            render json: {menuItem: nil, user: @user, auth: true , error: true , heading: "Opps...", message: "We ran into an error loading your selection. Please try again."}
         end
     end
 
     def create
         @item = MenuItem.create(menu_params)
         if @item.valid?
-            render json: {menuItems: @item, user: @user, auth: true, error: false, heading: "New Item!", message: "A new item has been added to your collection."}
+            render json: {menuItem: @item, user: @user, auth: true, error: false, heading: "New Item!", message: "A new item has been added to your collection."}
         else
-            render json: {menuItems: null, user: @user, auth: true, error: true, heading: "Opps...", message: "There was an issue creating your menu item. Please try again."}
+            render json: {menuItem: nil, user: @user, auth: true, error: true, heading: "Opps...", message: "There was an issue creating your menu item. Please try again."}
         end
     end
     
     def update
         @item = MenuItem.find(params[:id])
         if @item.update(menu_params)
-            render json: {menuItems: @item, user: @user, auth: true, error: false, heading:"Updated!", message: "The menu option was successfully updates!"}
+            render json: {menuItem: @item, user: @user, auth: true, error: false, heading:"Updated!", message: "The menu option was successfully updates!"}
         else
-            render json: {menuItems: @item , user: @user, auth: true, error: true, heading:"Opps..", message: "There was an issue updating your menu item. Please try again."}
+            render json: {menuItem: @item , user: @user, auth: true, error: true, heading:"Opps..", message: "There was an issue updating your menu item. Please try again."}
         end
     end
     
     def destroy
         @item = MenuItem.find(params[:id])
         if @item.destroy
-            render json: {menuItems: null, user: @user, auth: true, error: false, heading: "MenuItem Deleted!", message: "The item yo requested deleted was successfully removed."}
+            render json: {menuItem: nil, user: @user, auth: true, error: false, heading: "MenuItem Deleted!", message: "The item yo requested deleted was successfully removed."}
         else
-            render json: {menuItems: @item, user: @user, auth: true, error: true ,heading: "Opps...", message: "We can into an issue deleting your menu item."}
+            render json: {menuItem: @item, user: @user, auth: true, error: true ,heading: "Opps...", message: "We can into an issue deleting your menu item."}
         end
     end
 
     private
     
     def menu_params
-        params.require(:menu_item).permit( :title, :category, :descripton, :user_id)
+        params.require(:menu_item).permit( :title, :category, :description, :user_id)
     end
 end
